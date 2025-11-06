b779c8a2dc735eb5eeaa554a78ab7636750ae5d1
AoiKubota <aoi.yamamoto0506@gmail.com>
2025-11-06 23:18:46 +0900

    Delete planvista/planvista/main directory

--- planvista/planvista/main/resources/db/migration/V6_add_task_id_to_schedules.sql
+++
@@ -1,9 +0,0 @@
--- schedulesテーブルにtask_idカラムを追加
-
-ALTER TABLE schedules 
-ADD COLUMN task_id BIGINT COMMENT 'タスクID' AFTER user_id,
-ADD INDEX idx_task_id (task_id);
-
--- user_idの型をVARCHAR(10)からBIGINTに変更（将来的な互換性のため）
--- ※既存データがある場合は、事前にバックアップを取ってから実行してください
--- ALTER TABLE schedules MODIFY COLUMN user_id BIGINT NOT NULL COMMENT 'ユーザーID';
